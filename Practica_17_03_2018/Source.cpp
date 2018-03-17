5fe070b954fc1d6e4d88437c12b2f1feb54d3982
ТолстовА <@224_7_>
2018-03-17 10:58:21 +0600

    Add project files.

---
+++ Practica_17_03_2018/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+
+void main() {
+	
+	setlocale(LC_ALL, "Rus");
+
+	printf("¬ведите номер задани€")
+
+	char as[50];
+	printf("¬ведите строку: ");
+
+	char *pt = gets_s(as);
+
+	puts(as);
+
+
+	system("pause");
+}
